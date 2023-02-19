73f8a8d0f4ac6aef9c2f8e9e48ac852fc00647e9
VICTOR <vjcode091@gmail.com>
2023-02-19 04:39:30 -0800

    new commit with js

---
+++ script.js
@@ -0,0 +1,6 @@
+const vick = document.getElementById("myBtn");
+vick.addEventListener("click", sunday);
+
+function sunday() {
+    document.getElementById("demo").innerHTML = "completion of my git setup as been approved";
+}
